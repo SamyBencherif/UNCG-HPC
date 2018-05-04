e9f90df3a62a8d45470195ebfc38c08334ae3ff8
Samy Bencherif <samybencherif@live.com>
2018-05-04 11:26:32 -0400

    planning CH scriptgen

--- scriptgen.js
+++ scriptgen.js
@@ -19,6 +19,36 @@ function getHenryLFS(options) {
 
 }
 
+function getCHSlurm(options) {
+	/*
+	#!/bin/bash
+	#SBATCH --job-name=first_slurm_job //does henry need job names?
+	#SBATCH --ntasks=CPU_Count
+	#SBATCH --time=Wall_Minutes
+	#SBATCH --mem=MB_Per_Core
+
+	python <your python code>
+	*/
+
+	//export-file=<filename | fd>
+
+	//SLURM reference https://slurm.schedmd.com/sbatch.html
+
+	/*
+	-o, --output=<filename pattern>
+	Instruct Slurm to connect the batch script's standard output directly to the file name specified in the
+	"filename pattern". By default both standard output and standard error are directed to the same file.
+	For job arrays, the default file name is "slurm-%A_%a.out", "%A" is replaced by the job ID and "%a" with
+	the array index. For other jobs, the default file name is "slurm-%j.out", where the "%j" is replaced by the
+	job ID. See the filename pattern section below for filename specification options.
+	*/
+
+	/* Notes
+	- stdout/stderr go to the same file
+	- what is a job array and do I need to support it?
+	*/
+}
+
 //thank you https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
 function download(filename, text) {
 	var element = document.createElement('a');
