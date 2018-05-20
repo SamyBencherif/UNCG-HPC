bfe14f151a4d996eb38d24a471e2dd161a49d7fb
Samy Bencherif <samybencherif@live.com>
2018-05-19 21:35:44 -0400

    initial commit

---
+++ HPC-Project/src/build-graph.py
@@ -0,0 +1,20 @@
+#
+# MAG Interoperability Network Builder
+#
+# (c) Samy Bencherif
+# University of North Carolina Greensboro
+#
+
+class Paper:
+
+    def __init__(self):
+        pass
+
+
+class NetworkBuilder:
+
+    def __init__(self):
+        pass
+
+    def applyNextPaper(self):
+        pass
